93486cf74a0337b13a67e89a2688607efe44b472
Waleed Shoaib <87224462+waleedshoaib2@users.noreply.github.com>
2024-10-09 22:04:39 +0500

    Update pdf.module.ts

--- backend/src/pdf/pdf.module.ts
+++ backend/src/pdf/pdf.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PdfController } from './pdf.controller';
 import { PdfService } from './pdf.service';
-import { PromptModule } from '../prompt/prompt.module'; // Import the PromptModule
+import { PromptModule } from '../prompt/prompt.module'; 
 
 @Module({
   controllers: [PdfController],
